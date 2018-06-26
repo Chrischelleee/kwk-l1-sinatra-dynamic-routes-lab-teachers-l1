60f3a0133fc029f545d7c5a03e718df42edcc244
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-26 15:01:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+      get '/reversename/:name' do
+        @user_name = params[:name]
+        "#{@user_name.reverse}"
+      end
 
+      get '/square/:number' do
+        @num = params[:number].to_i * params[:number].to_i
+        "#{@num}"
+      end
+
+      get '/say/:number/:phrase' do
+        @phrase = params[:phrase]
+        until @phrase == 3
+          puts "show me the money"
+          break
+        end
+      end
 end
