712bc7bb6b3beb30278e3d305c9631917141f901
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-26 15:48:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,10 +17,10 @@ class App < Sinatra::Base
         (params[:number].to_i).times do
           string += "#{params[:phrase]}"
         end
-        string   
+        string
       end
 
       get '/say/:word1/:word2/:word3/:word4/:word5' do
-        
+        "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
       end
 end
