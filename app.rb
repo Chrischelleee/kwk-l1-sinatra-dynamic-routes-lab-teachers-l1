74437acac7dc1735490eddc714426b4f8738a8b3
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-26 15:53:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,10 +13,29 @@ class App < Sinatra::Base
       end
 
       get '/say/:number/:phrase' do
-        @phrase = params[:phrase]
-        until @phrase == 3
-          puts "show me the money"
-          break
+        string = ""
+        (params[:number].to_i).times do
+          string += "#{params[:phrase]}"
         end
+        string
+      end
+
+      get '/say/:word1/:word2/:word3/:word4/:word5' do
+        "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+      end
+
+      get '/:operation/:number1/:number2' do
+          @operation = params[:operation]
+          @num1 = params[:number1].to_i
+          @num2 = params[:number2].to_i
+          if @operation == "add"
+            "#{@num1 + @num2}"
+          elsif @operation == "subtract"
+            "#{@num1 - @num2}"
+          elsif @operation == "multiply"
+            "#{@num1 * @num2}"
+          elsif @operation == "divide"
+            "#{@num1 / @num2}"
+          end
       end
 end
